f6c693a65610bb52453049925b3f91705ed881bb
Ry <OfficiallyRy@outlook.com>
2023-02-04 02:03:48 -0700

    ActiveNote template all written, SetActiveNote and drawNote written

--- app/Controllers/NotesController.js
+++ app/Controllers/NotesController.js
@@ -1,17 +1,27 @@
 import { appState } from "../AppState.js";
 import { notesService } from "../Services/NotesService.js";
 import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
 
 function _drawNotes(){
 
 }
 
 function _drawNote(){
-  
+  // NOTE document.getElementById('noteTitle').style.color = "this.color";
+  let activeNote = appState.note
+  setHTML('centerPage', activeNote.ActiveNoteTemplate)
+  document.getElementById('noteTitle').style.color = appState.note.color
 }
 
 export class NotesController{
   constructor(){
+    _drawNotes
+  }
 
+  SetActiveNote(noteID){
+    let activeNote = appState.notes.find(note => note.id == noteID)
+    appState.note = activeNote
+    _drawNote()
   }
 }
--- app/Models/Note.js
+++ app/Models/Note.js
@@ -6,7 +6,32 @@ export class Note{
     this.id = generateId()
     this.title = data.title
     this.color = data.color
-    this.data = data.date || new Date().toLocaleDateString('en-US')
+    this.date = data.date || new Date().toLocaleDateString('en-US')
     this.body = data.body || ''
+    this.lastUpdated = data.lastUpdated
+  }
+
+  get ActiveNoteTemplate(){
+    return `
+  <div class="container-fluid hundo">
+    <div class="row hundo pb-5" id="noteRow">
+      <div class="col-8 offset-2 noteCard rounded hundo">
+        <div class="row hundo">
+          <div class="col-4 ps-4 pt-4">
+            <h2 id="noteTitle">${this.title}</h2>
+            <h5>Date Created: ${this.date}</h5>
+            <h5>Last Updated: ${this.lastUpdated}</h5>
+          </div>
+          <div class="col-6 d-flex align-items-center justify-content-center g-0">
+            <textarea name="body" id="noteText" cols="100" rows="22" class="rounded" placeholder="Your notes go here!">${this.body}</textarea>
+          </div>
+          <div class="col-2 text-end pt-4 pe-4">
+            <button id="deleteNote" onclick="app.notesController.deleteNote(${this.id})"><i class="mdi mdi-trash-can fs-2"></i></button>
+          </div>
+          </div>
+        </div>
+      </div>
+  </div>
+    `
   }
 }
